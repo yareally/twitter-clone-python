bc0652fcdc163358172c27301e151d4a0ce7ce5f
tflemings <umadbro554@yahoo.com>
2013-11-09 15:11:35 -0500

    Removed the calls to app.config

--- controllers/login.py
+++ controllers/login.py
@@ -15,27 +15,27 @@ def login(app):
         password = request.form['password']
 
         # should be verifying via the redis wrapper methods, not using app.*
-        if name != app.config['name']:
-            error.append('Invalid Username')
-        elif password != app.config['password']:
-            error.append('Invalid Password')
+        # if name != app.config['name']:
+        #     error.append('Invalid Username')
+        # elif password != app.config['password']:
+        #     error.append('Invalid Password')
+        # else:
+        # Set the session as logged in.
+        user = ''
+        redis = StrictRedis()
+        dbh = UserHelper(redis)
+        if dbh.email_exists(name):
+            user = dbh.get_user_by_email(request.form['name'])
+        elif dbh.username_exists(name):
+            user = dbh.get_user_by_username(request.form['name'])
+        if user != '':
+            hashed_password = UserHelper.hash_password(password, user.salt)
+            if hashed_password == user.password:
+                # Save the user to the session
+                session['user'] = user
+                return render_template('dash.html', title='Welcome to Twic')
         else:
-            # Set the session as logged in.
-            user = ''
-            redis = StrictRedis()
-            dbh = UserHelper(redis)
-            if dbh.email_exists(name):
-                user = dbh.get_user_by_email(request.form['name'])
-            elif dbh.username_exists(name):
-                user = dbh.get_user_by_username(request.form['name'])
-            if user != '':
-                hashed_password = UserHelper.hash_password(password, user.salt)
-                if hashed_password == user.password:
-                    # Save the user to the session
-                    session['user'] = user
-                    return render_template('dash.html', title='Welcome to Twic')
-            else:
-                error.append('That user does not exist.')
+            error.append('That user does not exist.')
 
         return render_template('login.html', error=error)
 
