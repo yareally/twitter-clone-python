f7c18304e665158b0fa28822bec128203e6b6d29
Wes <lannin05@email.franklin.edu>
2013-11-09 13:00:06 -0500

    -- Added some calls to fetch followers as well as add a follower
    -- Added unit tests showing adding a follower works
    
    -- Fixed a bug where the salt was not matching as well as a unit test showing it matches

--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
 # coding=utf-8
-from collections import OrderedDict
-import os
 
+import os
 
 
 class User(object):
@@ -13,23 +12,29 @@ class User(object):
     @param name:
     """
 
-    _values = dict()
-    messages = OrderedDict()
-
     USER_ID_KEY = 'id'
     USERNAME_KEY = 'username'
     EMAIL_KEY = 'email'
     PASS_KEY = 'password'
     NAME_KEY = 'name'
-    SALT = 'salt'
+    SALT_KEY = 'salt'
+    FOLLOWERS_ID_KEY = 'followers'
+    FOLLOWING_ID_KEY = 'following'
+    POSTS_ID_KEY = 'posts'
 
     def __init__(self, username='', email='', password='', name='', salt=''):
+        self._values = dict()
+
         self._values[self.USERNAME_KEY] = username
         self._values[self.EMAIL_KEY] = email
         self._values[self.NAME_KEY] = name
-        self._values[self.PASS_KEY] = password
-        self._values[self.SALT] = salt if salt else bytes(os.urandom(24).encode('base_64'))
+        self._values[self.PASS_KEY] = bytes(password)
+        self._values[self.SALT_KEY] = salt if salt else bytes(os.urandom(24).encode('base_64'))
         self._values[self.USER_ID_KEY] = 0
+
+        self.followers = set()
+        self.following = set()
+        self.messages = list()
         # self.token = Token()
 
     @property
@@ -42,11 +47,11 @@ class User(object):
 
     @property
     def password(self):
-        return self._values[self.PASS_KEY]
+        return str(self._values[self.PASS_KEY])
 
     @property
     def salt(self):
-        return self._values[self.SALT]
+        return self._values[self.SALT_KEY]
 
     @property
     def name(self):
@@ -58,9 +63,17 @@ class User(object):
 
     @id.setter
     def id(self, value):
+        """
+        @param value: The id for the user
+        @type value: int
+        """
         self._values[self.USER_ID_KEY] = value
 
     def items(self):
+        """
+        @return: dictionary of all properties for the user
+        @rtype: dict
+        """
         return self._values.items()
 
     def __iter__(self):
--- tests/redistests.py
+++ tests/redistests.py
@@ -25,12 +25,26 @@ class RedisTests(unittest.TestCase):
         self.assertTrue(self.dbh.username_exists(self.user.username))
         self.assertTrue(self.dbh.user_id_exists(self.user.id))
 
-    def get_user(self):
-        """
-        Test to see if a user can be retrieved from redis.
-        """
         stored_user = self.dbh.get_user_by_id(self.user.id)
-        self.assertEqual(self.user, stored_user)
+        self.assertEqual(self.user.email, stored_user.email)
+        self.assertEqual(self.user.username, stored_user.username)
+        self.assertEqual(str(self.user.id), str(stored_user.id))
+        self.assertEqual(self.user.salt, stored_user.salt)
+        self.assertEqual(self.user.name, stored_user.name)
+        self.assertEqual(self.user.followers, stored_user.followers)
+        self.assertEqual(self.user.following, stored_user.following)
+        self.assertEqual(self.user.messages, stored_user.messages)
+
+        passwd = UserHelper.hash_password(self.user.password, stored_user.salt)
+        self.assertEqual(passwd, stored_user.password)
+
+    def test_add_follower(self):
+        """
+        Test adding and getting a follower
+        """
+        self.dbh.add_follower(2, self.user.id)
+        followers = self.dbh.get_follower_ids(self.user.id)
+        self.assertTrue('2' in followers)
 
     def tearDown(self):
         """
@@ -39,7 +53,9 @@ class RedisTests(unittest.TestCase):
         @return:
         @rtype:
         """
-        self.dbh.delete_user(self.user.id)
+        if self.dbh.user_id_exists(self.user.id):
+            self.dbh.delete_user(self.user.id)
+
         self.assertFalse(self.dbh.user_id_exists(self.user.id))
         self.assertFalse(self.dbh.email_exists(self.user.email))
         self.assertFalse(self.dbh.username_exists(self.user.username))
