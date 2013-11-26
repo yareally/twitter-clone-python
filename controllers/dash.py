2e872061486f39ba241f2590dc8493db2a91a87a
tflemings <umadbro554@yahoo.com>
2013-11-25 21:10:17 -0500

    Began adding messages to the dash

--- controllers/dash.py
+++ controllers/dash.py
@@ -3,6 +3,8 @@ from flask import Flask, request, render_template, session
 from redis import StrictRedis
 import os
 from libs.rediswrapper import UserHelper
+from libs.rediswrapper import MessageHelper
+from models.message import Message
 from models.user import User
 
 
@@ -17,29 +19,16 @@ def dash(app):
 
     if request.method == 'GET':
         if session.has_key('user'):
-
-            return render_template('dash.html', error=None, title='Twic Dashboard')
+            dbh = MessageHelper(StrictRedis())
+            messages = dbh.get_user_messages(session['user']['id'])
+            message = Message(6, 'This is a message to check if it gets rendered in the dash.')
+            messages.append(message)
+            return render_template('dash.html', error=None, title='Twic Dashboard', messages=messages)
         else:
             return render_template('login.html', error='Please log in', title='Login')
     elif request.method == 'POST':
         error = list()
-        name = request.form['name']
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-        user = User(username, email, password, name)
         dbh = UserHelper(StrictRedis())
-
-        if name == '' or username == '' or email == '' or password == '':
-            error.append('All fields are required')
-            return render_template('registration.html', error=error, title='Twic Registration', user=user)
-        elif dbh.email_exists(user.email) or dbh.username_exists(user.username):
-            error.append('That username already exists. Please choose another')
-            return render_template('registration.html', error=error, title='Twic Registration', user=user)
-        elif dbh.email_exists(user.email):
-            error.append('That email already exists. Please choose another')
-            return render_template('registration.html', error=error, title='Twic Registration', user=user)
-        else:
-            dbh.add_user(user)
-            session['user'] = user.__str__()
-            return render_template('dash.html', title='Twic Registration')
+        dbh.add_user(user)
+        session['user'] = user.__str__()
+        return render_template('registration.html', title='Twic Registration')
